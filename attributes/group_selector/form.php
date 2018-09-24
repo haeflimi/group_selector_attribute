15f0a0db5a6892e88530759ef864140e27812113
Michael Häfliger <michael.haefliger@mesch.ch>
2018-09-24 17:06:18 +0200

    properly name attribute form field

--- attributes/group_selector/form.php
+++ attributes/group_selector/form.php
@@ -1,5 +1,5 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");?>
-<select class="form-control" name="value">
+<select class="form-control" name="<?=$this->field('value')?>">
     <option><?=t('Select a Group')?></option>
     <?php foreach($group_list->getResults() as $group): ?>
         <option value="<?=$group->getGroupID()?>" <?php if($group->getGroupID()==$value)echo 'selected'?>><?=$group->getGroupPath()?></option>
